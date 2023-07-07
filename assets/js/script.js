2f236b1ac38c24b7744220e1ade47cdd69e9d062
cait <caitlinba27@gmail.com>
2023-07-06 20:10:46 -0500

    Moved charSet object into generatePassword function

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,18 @@ var generateBtn = document.querySelector("#generate");
 
 // this function gives the user various prompts asking them about the criteria of their password
 function generatePassword() {
+  // the following object stores the character set and its properties 
+  const charSet = {
+    // holds a string of uppercase letters
+    upperCase: "ABCDEFGHIJKLMNOP",
+    // holds a string of lowercase letters
+    lowerCase: "abcdefghijklmnop",
+    // holds a string of numbers 0-9
+    numbers: "123456789",
+    // holds common special characters used in secure passwords
+    special: "~!@#$%^&*()_-+="
+  }
+  
   var passwordLength = prompt("How long do you want your password to be? Please type a number between 8 and 128: ");
   
     // this if..else statement makes sure the user input a number that is not less than 8 or not more than 128
@@ -29,17 +41,7 @@ function generatePassword() {
 // Write password to the #password input
 function writePassword() {
   generatePassword();
-  // the following object stores the character set and its properties 
-  const charSet = {
-    // holds a string of uppercase letters
-    upperCase: "ABCDEFGHIJKLMNOP",
-    // holds a string of lowercase letters
-    lowerCase: "abcdefghijklmnop",
-    // holds a string of numbers 0-9
-    numbers: "123456789",
-    // holds common special characters used in secure passwords
-    special: "~!@#$%^&*()_-+="
-  }
+  
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
