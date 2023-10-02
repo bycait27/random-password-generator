dad5d527dda631703dcba7bf8164697b2f572fa3
Caitlin Ash <caitlinba27@gmail.com>
2023-10-02 13:11:48 -0500

    Fixed formatting of code and for loop

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,14 +8,16 @@ var lowerCase = "abcdefghijklmnopqrstuvwxyz";
 var numbers = "123456789";
 // makes a variable for special characters
 var special = "~!@#$%^&*()_-+=";
-// empty string for password
-var newPassword = "";
 
-// this function gives the user various prompts asking them about the criteria of their password
-function generatePassword() {
-  
-  // prompts user to enter a number between 8 and 128
-  var passwordLength = prompt("How long do you want your password to be? Please type a number between 8 and 128: ");
+// add event listener to 'generate password' button
+generateBtn.addEventListener("click", writePassword);
+
+// writes password to the text box
+function writePassword() {
+  // prompts user with password criteria
+  function generatePassword() {
+     // prompts user to enter a number between 8 and 128
+    var passwordLength = prompt("How long do you want your password to be? Please type a number between 8 and 128: ");
   
     // this if..else statement makes sure the user input a number that is not less than 8 or not more than 128
     // if this is true, their answer validated via an alert with their input displayed back to them
@@ -26,52 +28,53 @@ function generatePassword() {
         alert(`You entered: ${passwordLength}.`);
     };
 
-  // makes the user confirm whether or not they want lowercase letters in their password
-  var wantsLowerCase = confirm("Do you want your password to include lowercase characters?");
-  // makes the user confirm whether or not they want uppercase letters in their password
-  var wantsUpperCase = confirm("Do you want your password to include uppercase characters?");
-  // makes the user confirm whether or not they want numbers in their password
-  var wantsNumeric = confirm("Do you want your password to include numeric characters?");
-  // makes the user confirm whether or not they want special characters in their password
-  var wantsSpecial = confirm("Do you want your password to include special characters?");
+    // makes the user confirm whether or not they want lowercase letters in their password
+    var wantsLowerCase = confirm("Do you want your password to include lowercase characters?");
+    // makes the user confirm whether or not they want uppercase letters in their password
+    var wantsUpperCase = confirm("Do you want your password to include uppercase characters?");
+    // makes the user confirm whether or not they want numbers in their password
+    var wantsNumeric = confirm("Do you want your password to include numeric characters?");
+    // makes the user confirm whether or not they want special characters in their password
+    var wantsSpecial = confirm("Do you want your password to include special characters?");
 
-  // adds the contents of upperCase to the empty newPassword string if user says yes to this confirm statement
-  if (wantsUpperCase === true) {
-    newPassword += upperCase;
-  };
-  // adds the contents of lowerCase to the empty newPassword string if user says yes to this confirm statement
-  if (wantsLowerCase === true) {
-    newPassword += lowerCase;
-  };
-  // adds the contents of numbers to the empty newPassword string if user says yes to this confirm statement
-  if (wantsNumeric === true) {
-    newPassword += numbers;
-  };
-  // adds the contents of special to the empty newPassword string if user says yes to this confirm statement
-  if (wantsSpecial === true) {
-    newPassword += special;
-  };
+    // makes empty string for password
+    var newPassword = "";
+
+    // adds the contents of upperCase to the empty newPassword string if user says yes to this confirm statement
+    if (wantsUpperCase === true) {
+      newPassword += upperCase;
+    };
+    // adds the contents of lowerCase to the empty newPassword string if user says yes to this confirm statement
+    if (wantsLowerCase === true) {
+      newPassword += lowerCase;
+    };
+    // adds the contents of numbers to the empty newPassword string if user says yes to this confirm statement
+    if (wantsNumeric === true) {
+      newPassword += numbers;
+    };
+    // adds the contents of special to the empty newPassword string if user says yes to this confirm statement
+    if (wantsSpecial === true) {
+      newPassword += special;
+    };
+
+    // makes empty string for new password
+    var passwordFinal = "";
+
+    // this for loop iterates through the length of the user's passwordLength input and makes a random string from the characters
+    for (i = 0; i < passwordLength; i++) {
+      passwordFinal += newPassword.charAt(Math.floor(Math.random() * newPassword.length));
+      };
+
+      // return new password generated based on user's input
+      return passwordFinal;
   };
 
-function createPassword() {
-  // this for loop iterates through the length of the user's passwordLength input and makes a random string from the characters
-  for (i = 0; i < passwordLength; i++) {
-    var passwordFinal = newPassword.charAt(Math.floor(Math.random() * newPassword.length));
-  };
-}
+    // calls generatePassword function and gets the generated password
+    var password = generatePassword();
 
-// Write password to the #password input
-function writePassword() {
-  // calls generatePassword function
-  generatePassword();
-  // inputs the result of the createPassword function to the password variable
-  var password = createPassword();
-  //sets a variable for this element id
-  var passwordText = document.querySelector("#password");
-  // inputs the value of password variable into the text box
-  passwordText.value = password;
-}
+    // sets a variable for this element id
+    var passwordText = document.querySelector("#password");
 
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", generatePassword);
+    // inputs the value of password variable into the text box
+    passwordText.value = password;
+};
